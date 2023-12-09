b56aadfeaa4f11813de8fdf60064daab12f4c7d3
Cully Larson <cully.larson@gmail.com>
2023-12-09 13:31:51 -0800

    Add a day 8, part 2 solution that uses workers but still takes too long.

--- day-08/part-2.js
+++ day-08/part-2.js
@@ -1,38 +1,87 @@
+import {Worker, isMainThread, parentPort, workerData} from 'node:worker_threads'
 import {compose, report} from '@cullylarson/f'
 import {then} from '@cullylarson/p'
 import {readInput} from './lib.js'
-import {rel} from '../lib.js'
+import {rel, max} from '../lib.js'
 
-const followInstruction = (location, instruction, network) => network[location][instruction]
+const isDone = location => location.endsWith('Z')
 
-const isAllDone = locations => locations.every(x => x.endsWith('Z'))
+// all done when all locationItems have the same number of steps
+const isAllDone = locationItems => {
+  return Array.from(new Set(locationItems.map(x => x.steps))).length === 1
+}
 
-const followInstructions = ({instructions, network}) => {
+const runLocationItem = async (maxSteps, locationItem) => {
+  if(maxSteps !== 0 && locationItem.steps >= maxSteps) {
+    return locationItem
+  }
+
+  return new Promise((resolve, reject) => {
+    const worker = new Worker('./part-2.js', {
+      workerData: locationItem,
+    })
+
+    worker.on('message', resolve)
+    worker.on('error', reject)
+    worker.on('exit', (code) => {
+      if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`))
+    })
+  })
+}
+
+const runLocationItems = async (locationItems) => {
+  const maxSteps = max(locationItems.map(x => x.steps))
+
+  return Promise.all(locationItems.map(locationItem => runLocationItem(maxSteps, locationItem)))
+}
+
+const followInstructions = async ({instructions, network}) => {
   const nodes = Object.keys(network)
 
-  let locations = nodes.filter(x => x.endsWith('A'))
+  let locationItems = nodes
+    .filter(x => x.endsWith('A'))
+    .map(x => ({
+      i: -1,
+      location: x,
+      instructions,
+      network,
+      steps: 0,
+    }))
 
-  let i = 0
-  let steps = 0
+  do {
+    locationItems = await runLocationItems(locationItems)
+    console.log(locationItems.map(x => x.steps)) // stub
+  } while(!isAllDone(locationItems))
 
-  while(!isAllDone(locations)) {
-    const instruction = instructions[i]
+  return locationItems[0].steps
+}
 
+const followInstructionForOneLocation = ({i, location, instructions, network, steps}) => {
+  do {
     i++
 
     if(i >= instructions.length) {
       i = 0
     }
 
-    locations = locations.map(location => followInstruction(location, instruction, network))
+    const instruction = instructions[i]
+
+    location = network[location][instruction]
 
     steps++
-  }
+  } while(!isDone(location))
 
-  return steps
+  return {i, location, instructions, network, steps}
 }
 
-then(compose(
-  report,
-  followInstructions,
-), readInput(rel(import.meta.url, 'input.txt')))
+if(isMainThread) {
+  then(compose(
+    then(report),
+    followInstructions,
+  ), readInput(rel(import.meta.url, 'input.txt')))
+}
+else {
+  const result = followInstructionForOneLocation(workerData)
+
+  parentPort.postMessage(result)
+}
